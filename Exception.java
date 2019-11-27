cbaee07817a4d99f0a58a09b3402bf3c2a0feb12
impatidar <pankaj_patidar@hotmail.com>
2019-11-28 00:50:38 +0530

    Exception Handling in Java.

---
+++ Exception.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static boolean flag = false;
+    static Scanner sc = new Scanner(System.in);
+    public static int B = sc.nextInt();
+    public static int H = sc.nextInt();
+
+    static {
+        if (B > 0 && H > 0) {
+            flag = true;
+        } else {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        if (flag) {
+            int area = B * H;
+            System.out.print(area);
+        }
+
+    }//end of main
+
+}//end of class
