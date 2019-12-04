8ef2b0b69b4543f4740eb961bf321e1d61f8e998
impatidar <pankaj_patidar@hotmail.com>
2019-12-05 02:53:49 +0530

    String Tokens in Java

--- src/stringTokens.java
+++ src/stringTokens.java
@@ -1,3 +1,31 @@
+
+import java.util.*;
+
 public class stringTokens {
 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        String[] items = s.trim().split("[ !,?.\\_'@]+");
+
+
+        if (s == null || s.trim().equals("")) {
+            System.out.println("0");
+        }
+        else if(s == ""){
+            System.out.println("0");
+        }
+        else if(s.length() > 400000){
+            return;
+        }
+        else{
+            System.out.println(items.length);
+        }
+
+        for(String item: items){
+            System.out.println(item);
+        }
+        scan.close();
+    }
 }
+
