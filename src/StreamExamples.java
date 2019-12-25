c021cc0adfcad15b26b615ddcf1b08d3ce9c6f0a
impatidar <pankaj_patidar@hotmail.com>
2019-12-26 01:35:38 +0530

    Stream Examples 2

--- src/StreamExamples.java
+++ src/StreamExamples.java
@@ -2,9 +2,16 @@ import java.util.stream.IntStream;
 
 public class StreamExamples {
     public static void main(String[] args) {
-        IntStream
-                .range(1,10)
-                .forEach(System.out::print);
+        // Skip Method
+//        IntStream
+//                .range(1,10)
+//                .skip(5)
+//                .forEach(x -> System.out.println(x));
+
+        //Sum Method
+        System.out.println(IntStream
+                .range(1, 5)
+                .sum());
         System.out.println();
     }
 }
